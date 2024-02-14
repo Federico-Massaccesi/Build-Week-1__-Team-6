3fe8e4ee3a2ffac44d0ce417885325aaac995cf1
LuigiSurio1 <luigisurio1@gmail.com>
2024-02-14 16:11:57 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 let circleProgress = document.querySelector('.circle'),
 centro = document.querySelector('.Centro');
 
+let centroStartValue = 0,
+    centroEndValue = 66,
+    speed = 100;
+
+let progress = setInterval(() => {
+    centroStartValue++;
+
+    circleProgress.style.background = `conic-gradient(#00FFFF ${centroStartValue * 3.6}deg, #D20094 0deg)`
+
+    if(centroStartValue === centroEndValue){
+        clearInterval(progress);
+    }
+}, speed);
+
 
