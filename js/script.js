f6f0b3f63480baa37c69d6064bcfb3ba63dae1df
Danilon6 <danilonebulosi@gmail.com>
2024-02-14 14:22:35 +0100

    fix_format

--- js/script.js
+++ js/script.js
@@ -14,13 +14,13 @@ let divResultsPage = document.querySelector("#Results-page")
 let divfeedbackPage = document.querySelector("#Feedback-page")
 
 //SELEZIONE CHECKBOX
-let checkbox= document.querySelector('#agree_terms')
+let checkbox = document.querySelector('#agree_terms')
 
 //SELEZIONE DIV CHECKBOX
 
 let divCheck = document.querySelector('#div-check')
 
-let pages =[divBenchmarkPage, divResultsPage, divfeedbackPage]
+let pages = [divBenchmarkPage, divResultsPage, divfeedbackPage]
 
 for (let i = 0; i < pages.length; i++) {
 
@@ -31,14 +31,14 @@ for (let i = 0; i < pages.length; i++) {
 button.addEventListener("click", display)
 
 function display() {
-    if(checkbox.checked){
-    divWelcomePage.classList.add("display")
-    divBenchmarkPage.classList.remove("display")
-}else{
-    divCheck.innerText = "Campo obbligatorio!"
-    divCheck.style.marginLeft = "5px"
-    divCheck.style.color = "red"
-}
+    if (checkbox.checked) {
+        divWelcomePage.classList.add("display")
+        divBenchmarkPage.classList.remove("display")
+    } else {
+        divCheck.innerText = "Campo obbligatorio!"
+        divCheck.style.marginLeft = "5px"
+        divCheck.style.color = "red"
+    }
 }
 //BENCHMARK PAGE
 
@@ -56,72 +56,72 @@ let benchmarkBtn4 = document.querySelector('#risp4')
 
 //ARRAY BUTTON
 
-let allButtonBenchmark = [benchmarkBtn1,benchmarkBtn2,benchmarkBtn3,benchmarkBtn4]
+let allButtonBenchmark = [benchmarkBtn1, benchmarkBtn2, benchmarkBtn3, benchmarkBtn4]
 
- //div che contiene h2 con la domanda
+//div che contiene h2 con la domanda
 
-let domanda = document.querySelector('.scritte') 
+let domanda = document.querySelector('.scritte')
 
 let punteggio = 0
 
 let questionNumber = document.querySelector('.nDomande')
 
 fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
-  .then(response => response.json())
-  .then(dati => {
+    .then(response => response.json())
+    .then(dati => {
 
-    console.log(dati.results);
-    
-    function mandaDomande(){
-    
-    let random = dati.results[Math.floor(Math.random()*dati.results.length )]
-    
-    let domandaSingola = random.question
-    
-    let testo = document.createElement('h2')
-    
-    let h2 = document.querySelector('h2')
-    
-    h2.innerHTML = domandaSingola
-    
-    domanda.append(h2)
-    
-    /*do {
-        domandaSingola
-    } while (domandeUscite.includes(domandaSingola));
-    domandeUscite.push(domandaSingola)
-    return domandeUscite*/
+        console.log(dati.results);
 
-    for (let i = 0; i < allButtonBenchmark.length; i++) {
-        
-        allButtonBenchmark[i].addEventListener('click',buttonColor)
-        
-        function buttonColor(){
-    
-            allButtonBenchmark[i].style.backgroundColor = '#D20094'
+        function mandaDomande() {
 
-            setTimeout(function() {
-    
-                mandaDomande();
-        
-                setTimeout(function() {
-    
-                    allButtonBenchmark[i].style.backgroundColor = ''; // Ripristina il colore del pulsante
-                }, 0); // Intervallo di 1 secondo per ripristinare il colore del pulsante
-            }, 800);
-            
+            let random = dati.results[Math.floor(Math.random() * dati.results.length)]
+
+            let domandaSingola = random.question
+
+            let testo = document.createElement('h2')
+
+            let h2 = document.querySelector('h2')
+
+            h2.innerHTML = domandaSingola
+
+            domanda.append(h2)
+
+            /*do {
+                domandaSingola
+            } while (domandeUscite.includes(domandaSingola));
+            domandeUscite.push(domandaSingola)
+            return domandeUscite*/
+
+            for (let i = 0; i < allButtonBenchmark.length; i++) {
+
+                allButtonBenchmark[i].addEventListener('click', buttonColor)
+
+                function buttonColor() {
+
+                    allButtonBenchmark[i].style.backgroundColor = '#D20094'
+
+                    setTimeout(function () {
+
+                        mandaDomande();
+
+                        setTimeout(function () {
+
+                            allButtonBenchmark[i].style.backgroundColor = ''; // Ripristina il colore del pulsante
+                        }, 0); // Intervallo di 1 secondo per ripristinare il colore del pulsante
+                    }, 800);
+
+                }
+
+
+            }
+
+
         }
-        
-        
-    }
 
-
-}
-
-mandaDomande();
+        mandaDomande();
 
 
 
 
 
-})
+    })
