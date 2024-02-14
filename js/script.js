882bde05a72e011a779aedeb95a9355ffb9b8977
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-02-14 17:29:40 +0100

    beta 1 timer

--- js/script.js
+++ js/script.js
@@ -18,16 +18,24 @@ seconds = "0" + seconds;
 return seconds;
 }
 
-//aggiorna il tempo e il bordo
 function updateTimer() {
 // Diminuisce il tempo di un secondo
 timeLeft--;
 
 timer.textContent = formatTime(timeLeft);
 
+if (timeLeft < 60) {
+circle.classList.add("high");
+};
+if (timeLeft < 45) {
+circle.classList.add("mid");
+};
 if (timeLeft < 30) {
 circle.classList.add("medium");
-}
+};
+if (timeLeft < 15) {
+circle.classList.add("low");
+};
 if (timeLeft === 0) {
 clearInterval(interval);
 }
