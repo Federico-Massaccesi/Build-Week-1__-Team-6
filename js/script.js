208e6bb21a0369a2627b5136445c5f0f06dd730c
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-02-15 09:59:42 +0100

    Timer version 1.0

--- js/script.js
+++ js/script.js
@@ -22,24 +22,45 @@ function updateTimer() {
 // Diminuisce il tempo di un secondo
 timeLeft--;
 
-timer.textContent = formatTime(timeLeft);
+const scritte = document.querySelector(".inside")
 
+timer.textContent = formatTime(timeLeft);
+if (timeLeft < 60) {
+timer.classList.add("high");
+scritte.style.color = "rgb(3, 198, 3)"
+};
+
+if (timeLeft < 45) {
+timer.classList.add("mid");
+scritte.style.color = "#ffff00"
+};
+
+if (timeLeft < 30) {
+timer.classList.add("medium");
+scritte.style.color = "rgb(255, 149, 0)"
+}
+
+if (timeLeft < 15) {
+timer.classList.add("low");
+scritte.style.color = "red"
+};
 
 if (timeLeft === 0) {
 clearInterval(interval);
 }
 }
-
 let interval = setInterval(updateTimer, 1000);
 /*FINE SCRIPT TIMER*/
 
 
+
+/*INIZIO SCRIPT CERCHIO*/
 let circleProgress = document.querySelector('.cerchio'),
 centro = document.querySelector('.inside');
 
 let centroStartValue = 100,
     centroEndValue = 0,
-    speed = 1000;
+    speed = 600;
 
 let progress = setInterval(() => {
     centroStartValue--;
